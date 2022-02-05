676fa471b9fc3fa852e3823436c3356fc7682781
Tim Holy <tim.holy@gmail.com>
2022-02-05 08:13:19 -0600

    Fake "complement": it's really just copy

--- java-imglib2/src/main/java/Benchmarks.java
+++ java-imglib2/src/main/java/Benchmarks.java
@@ -1,61 +1,83 @@
 package benchmarks;
 
-import ij.ImageJ;
-import ij.ImagePlus;
-import ij.io.Opener;
-
 import java.io.File;
 
-import io.scif.config.SCIFIOConfig;
-import io.scif.config.SCIFIOConfig.ImgMode;
+import ij.ImageJ;
+//import ij.ImagePlus;
+// import ij.io.Opener;
+
+import io.scif.img.IO;
 import io.scif.img.ImgIOException;
-import io.scif.img.ImgOpener;
+
+import net.imglib2.Cursor;
 
 import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.img.Img;
-import net.imglib2.img.array.ArrayImgFactory;
-import net.imglib2.img.display.imagej.ImageJFunctions;
+// import net.imglib2.img.display.imagej.ImageJFunctions;
+import net.imglib2.type.Type;
+
+// import io.scif.config.SCIFIOConfig;
+// import io.scif.config.SCIFIOConfig.ImgMode;
+// import io.scif.img.ImgIOException;
+import io.scif.img.ImgOpener;
+
+
+// import net.imglib2.img.array.ArrayImgFactory;
+// import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.NumericType;
-import net.imglib2.type.numeric.integer.UnsignedByteType;
+// import net.imglib2.type.numeric.integer.UnsignedByteType;
 //import net.imglib2.img.planar.PlanarCursor;
-import net.imglib2.img.array.ArrayCursor;
+// import net.imglib2.img.array.ArrayCursor;
 
 public class Benchmarks
 {
     // within this method we define <T> to be a NumericType (depends on the type of ImagePlus)
     // you might want to define it as RealType if you know it cannot be an ImageJ RGB Color image
-    public Benchmarks(String workdir)
+    public < T extends NumericType< T > & NativeType< T > > Benchmarks(String workdir) throws java.io.IOException
     {
         File workDir = new File(workdir);
         File filesList[] = workDir.listFiles();
         ImgOpener imgOpener = new ImgOpener();
         for (File file : filesList) {
             // open a file with ImageJ
-            Img< UnsignedByteType > img = ( Img< UnsignedByteType > ) imgOpener.openImgs( file.getAbsolutePath() ).get(0);
+            Img< T > img = ( Img< T > ) imgOpener.openImgs( file.getAbsolutePath() ).get(0);
             int niter = 16;
             long startTime = System.nanoTime();
             for (int i = 0; i < niter; i++) {
                 invertArrayImage(img);
             }
             long endTime = System.nanoTime();
-            System.out.println("Invert, "+file+": "+String.valueOf((double)(endTime-startTime)/(niter*1000000))+"ms");
+
+            System.out.println("complement,"+removeExtension(file.getName())+","+String.valueOf((double)(endTime-startTime)/(niter*100000000)));
         }
+        System.out.println("All benchmarks completed");
     }
 
-    private void invertArrayImage(final Img<UnsignedByteType> img) {
-        //private void invertArrayImage(final Img<T extends NumericType< T > & NativeType< T > img) {
-          final ArrayCursor<UnsignedByteType> c =
-            (ArrayCursor<UnsignedByteType>) img.cursor();
-        while (c.hasNext()) {
-            final UnsignedByteType t = c.next();
-            final int value = t.get();
-            final int result = 255 - value;
-            t.set(result);
+    private < T extends NumericType< T > & NativeType< T > > Img<T> invertArrayImage(final Img<T> input) {
+        Img< T > output = input.factory().create( input );
+        Cursor<T> cursorInput  = input.cursor();
+        Cursor<T> cursorOutput = output.cursor();
+        while (cursorInput.hasNext()) {
+            cursorInput.fwd();
+            cursorOutput.fwd();
+            T val = cursorInput.get();
+            cursorOutput.get().set(val);
+            // cursorOutput.get().set(T.MAX_VALUE - val);
+            // cursorOutput.get().set(val.getClass().MAX_VALUE - val);
         }
+        return output;
     }
 
-    public static void main( String[] args )
+    public static String removeExtension(String fname) {
+        int pos = fname.lastIndexOf('.');
+        if(pos > -1)
+           return fname.substring(0, pos);
+        else
+           return fname;
+     }
+
+    public static void main( String[] args ) throws java.io.IOException
     {
         // open an ImageJ window
         new ImageJ();
